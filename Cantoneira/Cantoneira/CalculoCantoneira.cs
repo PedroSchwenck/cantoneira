12f54e713c202dcab098b96fe85516485ec00d65
PedroSchwenck <141786337+PedroSchwenck@users.noreply.github.com>
2023-08-29 19:17:19 -0300

    Add files via upload

---
+++ Cantoneira/Cantoneira/CalculoCantoneira.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cantoneira
+{
+    internal class CalculoCantoneira
+    {
+
+        public List<string> Cantoneira(int n)
+        {
+            List<string> valor = new List<string>();
+            for (int x = 1; x <= 1; x++)
+            {
+                string espaco = new string(' ', n + x);
+                string numero = "";
+
+                for (int y = 1; y <= n; y++)
+                {
+                    numero += y;
+
+                    valor.Add(numero);
+                }
+            }
+            return valor;
+        }
+    }
+}
---
+++ Cantoneira/Cantoneira/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using cantoneira;
+
+
+
+Console.Write("Entre com um valor inteiro: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+CalculoCantoneira calculo = new CalculoCantoneira();
+List<string> valor = calculo.Cantoneira(n);
+
+foreach (string list in valor)
+{
+    Console.WriteLine(list);
+}
